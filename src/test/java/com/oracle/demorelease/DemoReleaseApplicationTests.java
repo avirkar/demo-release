5085853990d9cd56ddeddc042b9fc130e601197c
Ajinkya Virkar <ajinkya.virkar@oracle.com>
2021-06-14 14:47:50 -0400

    first commit

---
+++ src/test/java/com/oracle/demorelease/DemoReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.demorelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
